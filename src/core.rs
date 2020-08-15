966a5512484e7f95003fdd18ba76a5d8426fe0a8
Uzdenov Karim <mrkarim777@gmail.com>
2020-08-15 13:21:21 +0300

    master

---
+++ src/core.rs
@@ -0,0 +1,152 @@
+extern crate colored;
+use colored::*;
+use serde::{Deserialize, Serialize};
+use walkdir::{WalkDir};
+use std::{fs,io::BufWriter, io::BufReader, io::Write,io::Read, fs::File, path::Path};
+use std::collections::VecDeque;
+use std::io::prelude::*;
+
+#[macro_export]
+macro_rules! info_message {
+    () => {
+        let message ="[info]";
+        println!("{}", message.green());
+    };
+    ($($arg:tt)*) => {
+        let message ="[info]";
+        print!("{} ",message.green());
+        println!($($arg)*);
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CommentStruct 
+{
+    file_name_extension: Vec<String>,
+    comments: Vec<String>,
+}
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+    .map(|l| l.expect("Could not parse line"))
+    .collect()
+}
+
+fn pirnt_json_settings(json_settings : &CommentStruct) {
+    
+    for name in json_settings.file_name_extension.iter(){
+        info_message!("{} {}","Settings file name extensions: ".bold().blue(),name.bold());
+    }
+    
+    for comment in json_settings.comments.iter(){
+        info_message!("{} {}","Comments from the settings file:".bold().blue(),comment.bold());
+    }
+}
+
+pub fn delete_comment_v2(path : &&str)->std::io::Result<()> {
+    let mut settings_fille = File::open("settings.json").expect("Unable to open");
+    let mut data = String::new();
+    settings_fille.read_to_string(&mut data)?;
+    
+    
+    let json_settings:CommentStruct = serde_json::from_str(&data).expect("Json was not well format");
+
+ 
+    pirnt_json_settings(&json_settings);
+
+    for extensions in json_settings.file_name_extension.iter() {
+        let walker = WalkDir::new(path).into_iter();
+        for entry in walker {
+            let entry = entry.unwrap();
+            let file_name = entry.path().file_name().unwrap().to_str().unwrap();
+            if file_name.ends_with(extensions) {
+                info_message!("{}",file_name);
+
+               
+    
+              let mut strings_buffer = lines_from_file(entry.path()); 
+
+                
+                for comm in json_settings.comments.iter() {
+                    strings_buffer.retain(|x| x != comm);
+                                   
+                 }
+
+                 
+                 let  new_file = File::create(entry.path()).expect("Unable create file");
+                 for comment in strings_buffer {
+                 let mut writer = BufWriter::new(&new_file);
+                 writeln!(&mut writer,"{}",comment)?;
+                
+                 }     
+                
+    
+    
+            }
+    
+            
+        }
+    }
+    println!("{}","-------------------------DONE-------------------------".green());
+    Ok(())
+}
+
+pub fn add_comment(path : &&str)->std::io::Result<()> {
+    let mut settings_fille = File::open("settings.json").expect("Unable to open");
+    let mut data = String::new();
+    settings_fille.read_to_string(&mut data)?;
+    
+    
+    let mut  json_settings:CommentStruct = serde_json::from_str(&data).expect("Json was not well format");
+
+   
+    pirnt_json_settings(&json_settings);
+    
+    
+    json_settings.comments.reverse();
+    for extensions in json_settings.file_name_extension.iter() {
+        let walker = WalkDir::new(path).into_iter();
+        for entry in walker {
+            let entry = entry.unwrap();
+            let file_name = entry.path().file_name().unwrap().to_str().unwrap();
+            if file_name.ends_with(extensions) {
+                info_message!("{}",file_name);
+               
+    
+               let mut file = fs::OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .read(true)
+                    .open(entry.path())
+                    .unwrap();
+                let mut temp_data = String::new();
+                
+                file.read_to_string(&mut temp_data)?;
+    
+                 let mut string_buffer: VecDeque<String> = VecDeque::new();
+                 
+                 string_buffer.push_back(temp_data);
+                 
+                 for comments in json_settings.comments.iter() {
+                    string_buffer.push_front(comments.clone());
+                 }
+                let new_file = File::create(entry.path()).expect("Unable create file");
+                for comment in string_buffer {
+                    let mut writer = BufWriter::new(&new_file);
+                    writeln!(&mut writer,"{}",comment)?;
+                }     
+                
+    
+    
+            }
+    
+            
+        }
+    }
+    println!("{}","-------------------------DONE-------------------------".green());
+    Ok(())
+
+
+}
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+extern crate comment_adder;
+extern crate clap;
+
+use comment_adder::core::*;
+use clap::{App,Arg};
+fn main() -> std::io::Result<()>  {
+    let matches = App::new("Test App")
+    .author("Uzdenov Karim")
+    .about("This programm adds the ability to automatically add comments to source files.")
+    .version("0.1.0")
+    .usage("CommnetAdder [--add_comment, --delate_comment] <FILE>")
+        .arg(
+            Arg::with_name("add")
+            .takes_value(true)
+            .long("add_comment")
+            .short("add")
+            .multiple(true)
+            .required(true)
+            .conflicts_with("del")
+            .help("Added a comment that is specified in settings.json")
+            .display_order(1)            
+        )
+        .arg(
+            Arg::with_name("del")
+            .takes_value(true)
+            .long("delete_comment")
+            .short("del")
+            .multiple(true)
+            .required(true)
+            .conflicts_with("add")
+            .help("Deleted a comment that is specified in settings.json")
+            .display_order(2)
+        )
+        .get_matches();
+
+
+    if let Some(ref in_file) = matches.value_of("add") {
+        add_comment(in_file)?;
+    }
+    
+    if let Some(ref in_file) = matches.value_of("del") {
+        delete_comment_v2(in_file)?;
+
+    }
+    
+
+    Ok(())
+}
